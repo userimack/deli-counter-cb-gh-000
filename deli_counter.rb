9d7ace293ee75702ff7530cd1ac903c17d9f08be
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 15:13:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 # Write your code here.
 
 def line(array)
+  print "The line is currently: "
   array.each_with_index do |person, index|
-    print "The line is currently:"
+    print "index + 1}"
+    
